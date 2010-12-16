64fe53e6ef6aeeb8a93a593a0dfcf408a85ffaa7
Jeff Casimir <jeff@casimircreative.com>
2010-12-16 15:54:03 -0500

    removed unused helpers

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -1,15 +1,4 @@
-module NavigationHelper
-  
-  def navigation_by_state
-    content_tag(:ul, 
-      Listing.available_states.collect do |state|
-        content_tag(:li, link_to(state, listings_path(:state => state)))
-      end)
-  end
-  
-  def navigation_by_city
-    
-  end
+module NavigationHelper  
   
   def listings_for_city(city)
     
