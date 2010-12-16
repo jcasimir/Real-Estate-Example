af943a9fa237ab3f9fe867de83c210b06494de24
Jeff Casimir <jeff@casimircreative.com>
2010-12-16 15:51:52 -0500

    Helper to build up titles for the index view based on the passed parameters

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -1,2 +1,14 @@
 module ListingsHelper
+
+  def listings_title(params)
+    base = "All Available Listings"
+    if params[:city] && params[:state].nil?
+      base += " in #{params[:city]}"
+    elsif params[:city] && params[:state]
+      base += " in #{params[:city]}, #{params[:state]}"
+    elsif params[:city].nil? && params[:state]
+      base += " in #{params[:state]}"
+    end
+    base  
+  end
 end
