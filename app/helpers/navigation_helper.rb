2e1ddab4bd73c36f7e2c7335243976b0c8ae7853
Jeff Casimir <jeff@casimircreative.com>
2010-12-16 16:17:16 -0500

    Rewriting links within listings

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -1,10 +1,14 @@
 module NavigationHelper  
   
-  def listings_for_city(city)
-    
+  def link_for_city(city)
+    link_to city, listings_path(:city => city)
   end
   
-  def listings_for_state(state)
-    
+  def link_for_city_with_state(city, state)
+    link_to "#{city}, #{state}", listings_path(:city => city, :state => state)
+  end
+  
+  def link_for_state(state)
+    link_to state, listings_path(:state => state)
   end
 end
